01a414626c64a0a6ebc165d937f24aa339eff0fe
Evgeny <djek.uait1998@gmail.com>
2023-01-25 02:24:12 +0300

    slider-footer

---
+++ src/blocks/slider/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".slider__swiper-wrapper").slick({
+    prevArrow: '.slider__swiper-button-prev',
+    nextArrow: '.slider__swiper-button-next',
+    slidesToShow: 2,
+    
+    
+  })
+
+})
+
+
+   
+  
